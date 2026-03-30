5f6125749bd9a2985000ba806cff0eb991bfff13
replitnew3abc <56450841-replitnew3abc@users.noreply.replit.com>
2026-03-30 10:49:51 +0000

    Restored to 'ef2653faa66543866ca9fa98d8f132dc9ddec77d'
    Replit-Restored-To: ef2653faa66543866ca9fa98d8f132dc9ddec77d

--- dist/win-unpacked/resources/app/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  setSessionState: (state) => ipcRenderer.send('session-state', state),
-  triggerShutdown: () => ipcRenderer.send('trigger-shutdown'),
-});
